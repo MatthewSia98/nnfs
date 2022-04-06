a0ced6a9919f77f080fd79552db019979befd669
Matthew Sia <msia075@uottawa.ca>
2022-04-06 00:55:23 -0400

    made categorical loss work for one-hot and labels

--- layers.py
+++ layers.py
@@ -7,9 +7,11 @@ class DenseLayer:
     MAX_START_BIAS = 0
 
     def __init__(self, prev_n_neurons, n_neurons):
-        self.weights = np.random.uniform(DenseLayer.MIN_START_WEIGHT, DenseLayer.MAX_START_WEIGHT, (prev_n_neurons, n_neurons))
-        self.biases = np.random.uniform(DenseLayer.MIN_START_BIAS, DenseLayer.MAX_START_BIAS, (1, n_neurons))
-
+        #self.weights = np.random.uniform(DenseLayer.MIN_START_WEIGHT, DenseLayer.MAX_START_WEIGHT, (prev_n_neurons, n_neurons))
+        #self.biases = np.random.uniform(DenseLayer.MIN_START_BIAS, DenseLayer.MAX_START_BIAS, (1, n_neurons))
+        self.weights = 0.01 * np.random.randn(prev_n_neurons, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+        
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases       
         return self.output 
--- losses.py
+++ losses.py
@@ -1,5 +1,30 @@
 import numpy as np
 
-class CategoricalCrossEntropyLoss:
+class Loss:
+    def calculate(self, prediction, target):
+        losses = self.forward(prediction, target)
+        batch_loss = np.mean(losses)
+        return batch_loss    
+
+class CategoricalCrossEntropyLoss(Loss):
+    def __init__(self, one_hot=False):
+        self.one_hot = one_hot
+
     def forward(self, prediction, target):
-        self.output = -np.sum(target * np.log(prediction))
+        clipped_prediction = np.clip(prediction, 1e-7, 1-1e-7)
+
+        if self.one_hot:
+            losses = -np.sum(target * np.log(clipped_prediction), axis=1)
+            self.output = losses   
+        else:
+            losses = -np.log(clipped_prediction[range(len(target)), target])
+            
+        return losses
+
+"""
+softmax_outputs = np.array([[0.7, 0.1, 0.2], [0.1, 0.5, 0.4], [0.02, 0.9, 0.08]])
+class_targets = np.array([0, 1, 1])
+one_hot_targets = np.array([[1, 0, 0], [0, 1, 0], [0, 1, 0]])
+print(-np.log(softmax_outputs[[0, 1, 2], one_hot_targets]))
+print(CategoricalCrossEntropyLoss().forward(softmax_outputs, one_hot_targets))
+"""
